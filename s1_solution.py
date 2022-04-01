cc342d1aec7cd0f854de633e6e09e618e8358737
seanatwa <seanatwa@gmail.com>
2022-04-01 11:19:30 -0400

    feature: refactoring with +=

--- s1_solution.py
+++ s1_solution.py
@@ -59,8 +59,8 @@ from collections import defaultdict
 def compute_ctr_0(aSearches, aClicks):  # assume O(m), O(n), result 0(m*n)) 
   hit_agg = defaultdict(int)
   for c in aClicks: #O(m)
-    hit_agg[c["search_id"]] = hit_agg[c["search_id"]] + 1 if c["position"] == 0 else 0
-  
+    hit_agg[c["search_id"]] += 1 if c["position"] == 0 else 0
+  #hit_agg[c["search_id"]] + 1 if c["position"] == 0 else 0
   site_agg = defaultdict(lambda: {'searches': 0, 'hits': 0}) #O(n)
   for s in aSearch:
     site_agg[s["site_id"]] = {"searches": 1 + site_agg[s["site_id"]]["searches"],
